1a87b908c6fbce820cbcfec2d32274c095e8b488
Luis Fernando Rojas González <luisfernando.lzc@hotmail.com>
2024-09-30 21:56:00 -0600

    Prueba formulario con angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Form1Component } from './form1/form1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'form1', component: Form1Component }
+];
---
+++ src/app/form1/form1.component.spec.ts
@@ -0,0 +1,25 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Form1Component } from './form1.component';
+
+describe('Form1Component', () => {
+  let component: Form1Component;
+  let fixture: ComponentFixture<Form1Component>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Form1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
