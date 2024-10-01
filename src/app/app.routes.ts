bb09216bc861b7413616dbf5751a9f5f881cd0ee
Luis Fernando Rojas González <luisfernando.lzc@hotmail.com>
2024-09-30 21:59:21 -0600

    Added redirect to form1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { Form1Component } from './form1/form1.component';
 
 export const routes: Routes = [
-    { path: 'form1', component: Form1Component }
+    { path: 'form1', component: Form1Component },
+    { path: '**', redirectTo: 'form1'}
 ];
